7a07a9cdb2fa766353e4075315d532f6d3981b9c
nganvo <1612414@student.hcmus.edu.vn>
2020-03-12 12:42:45 +0700

    commit all

--- demo/navigators/RootNavigation.js
+++ demo/navigators/RootNavigation.js
@@ -1,37 +1,41 @@
 import React from 'react';
-import { View, Text, Button, TouchableOpacity, } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import  MyCameras from '../../src/screens/MyCameras';
-import  CameraAddingScreen from '../../demo/screens/cameraAdding/CameraAddingScreen';
-import CameraPlaying from '../../src/screens/camera/CameraPlaying'
+import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MyCameras from '../../src/screens/MyCameras';
+import CameraAddingScreen from '../../demo/screens/cameraAdding/CameraAddingScreen';
+import CameraPlaying from '../../src/screens/camera/CameraPlaying';
+import LocalAlbum from '../../src/screens/localAlbum/LocalAlbum';
+import MyProfile from '../../src/screens/myProfile/MyProfile'
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const RootNavigation = () => (
     <NavigationContainer>
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeStackScreen} />
-            <Tab.Screen name="Settings" component={SettingsStackScreen} />
-            <Tab.Screen name="MyProfile" component={MyCamerasStackScreen} />
+            <Tab.Screen name="Home" component={HomeStackScreen}/>
+            <Tab.Screen name="Local Album" component={LocalAlbumStackScreen}/>
+            <Tab.Screen name="MyProfile" component={SettingsStackScreen}/>
         </Tab.Navigator>
     </NavigationContainer>
 );
 
-export { RootNavigation };
+export {RootNavigation};
 
-function DetailsScreen() {
+function DetailsScreen(props) {
+    let name = props && props.route && props.route.name|| '';
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Details!</Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Details ! {!!name && typeof name === 'string' && name}</Text>
         </View>
     );
 }
 
-function SettingsScreen({ navigation }) {
+function SettingsScreen({navigation}) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Settings screen</Text>
             <Button
                 title="Go to Details"
@@ -46,39 +50,17 @@ const HomeStack = createStackNavigator();
 function HomeStackScreen() {
     return (
         <HomeStack.Navigator>
-            <HomeStack.Screen name="MyCameras" component={MyCameras} />
+            <HomeStack.Screen name="MyCameras" component={MyCameras}/>
             <HomeStack.Screen
                 name="CameraAdding"
                 component={CameraAddingScreen}
-                options={{
-                    headerRight: () => (
-                        <Button
-                            onPress={() => {
-                                let {navigation} = this.props;
-                                navigation.goBack();
-                            }}
-                            title="Info"
-                            color="#fff"
-                        />
-                    ),
-                }}
             />
             <HomeStack.Screen
                 name="CameraPlaying"
                 component={CameraPlaying}
-                options={{
-                    headerRight: () => (
-                        <Button
-                            onPress={() => {
-                                let {navigation} = this.props;
-                                navigation.goBack();
-                            }}
-                            title="Info"
-                            color="#fff"
-                        />
-                    ),
-                }}
+                options={{}}
             />
+
         </HomeStack.Navigator>
     );
 }
@@ -88,15 +70,20 @@ const SettingsStack = createStackNavigator();
 function SettingsStackScreen() {
     return (
         <SettingsStack.Navigator>
-            <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-            <SettingsStack.Screen name="Details" component={DetailsScreen} />
+            <SettingsStack.Screen name="MyProfile" component={MyProfile}/>
+            <SettingsStack.Screen name="Details" component={DetailsScreen}/>
         </SettingsStack.Navigator>
     );
 }
 
-function MyCamerasStackScreen() {
+const LAlbumStack = createStackNavigator();
+
+function LocalAlbumStackScreen() {
     return (
-        <Stack.Screen name={'MyCameras'} component={MyCameras}/>
-    )
+        <LAlbumStack.Navigator>
+            <LAlbumStack.Screen name="LocalAlbum" component={LocalAlbum}/>
+            <LAlbumStack.Screen name="Details" component={DetailsScreen}/>
+        </LAlbumStack.Navigator>
+    );
 }
 
--- src/screens/MyCameras.js
+++ src/screens/MyCameras.js
@@ -1,54 +1,61 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    View,
-    Dimensions,
-    TouchableOpacity,
-    Text,
-    ScrollView
-} from 'react-native';
-
-const { width } = Dimensions.get('window');
-
+import React, {Component} from 'react';
+import {Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, ScrollView} from 'react-native';
 import CameraVideo from '../components/camera/CameraVideo';
-const sampleAvatar = 'https://www.voicesofyouth.org/sites/default/files/styles/thumbnail/public/pictures/2019-03/nature-3125912_960_720.jpg?itok=ROz3xnZJ';
 
+const {width} = Dimensions.get('window');
+
+const rtsp = 'rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream';
+// const rtsp = 'rtsp://10.10.144.206:7777/h264_ulaw.sdp';
+const sampleAvatar = 'https://www.voicesofyouth.org/sites/default/files/styles/thumbnail/public/pictures/2019-03/nature-3125912_960_720.jpg?itok=ROz3xnZJ';
 
 export default class MyCameras extends Component {
 
-    onPressAdd = () =>{
-        let navigation = this.props.navigation;
-        navigation.navigate('CameraAdding')
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{name: 'Camera 01', url: rtsp}, {name: 'Camera 02', url: rtsp}],
+        };
+    }
+
+    componentDidMount() {
 
     }
 
-    onPressCam = (name, source) => {
+    onPressAdd = () => {
+        let navigation = this.props.navigation;
+        navigation.navigate('CameraAdding');
+
+    };
+
+    onPressCam = (name, source) => () => {
         let {navigation} = this.props;
         navigation.navigate('CameraPlaying', {
             cameraName: name,
             source: source,
         });
-    }
+    };
 
     render() {
-        let rtsp = 'rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa'
-        let link = 'http://wmccpinetop.axiscam.net/mjpg/video.mjpg'
+
         return (
             <ScrollView containerStyle={styles.body}>
-                <TouchableOpacity style={styles.camera} onPress={this.onPressCam('camra 01', rtsp)}>
-                    <CameraVideo
-                        source={rtsp}
-                        name='Camera 01'
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.camera}>
-                    <CameraVideo
-                        source={rtsp}
-                        name='Camera 02'
-                    />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}
-                                  onPress={this.onPressAdd}
+                <FlatList
+                    containerStyle={{backgroundColor:'yellow'}}
+                    horizontal={true}
+                    data={this.state.data}
+                    keyExtractor={item => item.name}
+                    renderItem={({item}) =>
+                        <TouchableOpacity style={styles.camera} onPress={this.onPressCam(item.name, item.url)}>
+                            <CameraVideo
+                                source={item.url}
+                                name={item.name}
+                            />
+                        </TouchableOpacity>
+                    }
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={this.onPressAdd}
                 >
                     <Text style={styles.text}>Add Camera</Text>
                 </TouchableOpacity>
@@ -58,32 +65,32 @@ export default class MyCameras extends Component {
 }
 
 const styles = StyleSheet.create({
-    body:{
+    body: {
         flex: 1,
         padding: 6,
-        justifyContent:'center',
+        justifyContent: 'center',
         alignContent: 'center',
-        alignItems:'center',
-        marginBottom:6
+        alignItems: 'center',
+        marginBottom: 6,
     },
-    camera:{
-        padding:12,
-        flex:1,
+    camera: {
+        paddingHorizontal:12,
+        flex: 1,
         // height:200,
-        width: '100%',
+        width: width,
         // height:2000,
     },
     button: {
         height: 50,
-        backgroundColor:'#000',
-        width:100,
+        backgroundColor: '#000',
+        width: 100,
         borderRadius: 4,
-        alignItems:'center',
-        justifyContent:'center',
-        alignSelf:'center'
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
     },
 
-    text:{
-        color:'#fff'
-    }
-})
+    text: {
+        color: '#fff',
+    },
+});
